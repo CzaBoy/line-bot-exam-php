2723ca66b7ff0e2883ac11b4864ed654dcffad87
CzaBoy <aune_lovely@hotmail.com>
2018-05-14 16:56:06 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'tb80FWV5uM+EIqZHGtBxUNBNikzn/HBI5da01qex1ZyreyqIti6zXKevIAHxBlE5X6rohcz2n5NscGJZd9wn46c3b/Awm4zJHVToaBuzfRg/QLhRD01BJnNL5qUiL4xsnBstsmJWcYFcwMWngL6zmQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
